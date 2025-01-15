625af8a97e3d3f2d835d19e33482a71d7a14981d
syahrul <muhdsyahrulnizam123@gmail.com>
2025-01-15 17:58:01 +0800

    Added basic example

--- examples/basic-example/src/main.rs
+++ examples/basic-example/src/main.rs
@@ -1,7 +1,26 @@
-use ferrox::{agent::text_agent::TextAgent, Ferrox};
+use std::env;
+
+use ferrox::{
+    agent::{text_agent::TextAgent, NullAgent},
+    Ferrox,
+};
+use openai_api::models::{Model, OpenAIModel};
+
+const SYSTEM_PROMPT: &str = "You are an onchain trading assitant with native capability to pull data from coingecko dexscreener or birdseye.";
 
 //In this example, the bot will be connected to a single agent.
 //We also register some basic onchain functions for the agent to use.
-
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    //Make sure to set the TELOXIDE_TOKEN and OPENAI_API_KEY environment variables.
+    let api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
+    let decision_agent = TextAgent::<NullAgent>::new(
+        None,
+        SYSTEM_PROMPT.to_string(),
+        api_key,
+        Model::OpenAI(OpenAIModel::GPT40),
+    );
+    let ferrox = Ferrox::new(decision_agent);
+    ferrox.start().await;
+    //You can now chat wit the bot.
+}
